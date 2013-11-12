a79fa6f8e7f4c1810c3fda63d1f71af750207df9
Damien Del Russo <ddelruss@mac.com>
2013-11-11 22:17:19 -0600

    console now knows how to look up a stock price.

--- hello-node.js
+++ hello-node.js
@@ -1,3 +1,16 @@
 // Run this by installing node.js and then "node hello-node.js"
-console.log("Hello node!");
+var $ = require('jQuery');
 
+console.log("Hello node!");
+getData();
+
+function getData() {
+    var url = "http://query.yahooapis.com/v1/public/yql";
+    var symbol = "AMZN";
+    var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
+
+    $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
+        .done(function (data) {
+		console.log(symbol + " bid price: " + data.query.results.quote.LastTradePriceOnly)
+    });
+}
