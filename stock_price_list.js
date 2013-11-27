7cfdd135f5d9015145ad4bdc328ea1fc74785c41
Damien Del Russo <ddelruss@mac.com>
2013-11-27 11:35:43 -0800

    Frankenmethod knows how to create weekly quotes.

--- stock_price_list.js
+++ stock_price_list.js
@@ -8,7 +8,7 @@ function getData() {
     var symbol = "AMZN";
     // var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
 	var dateComps;
-	var startDate = '2013-01-01';
+	var startDate = '2013-11-01';
 	var endDate = '2013-11-26';
 	var data = encodeURIComponent('select * from yahoo.finance.historicaldata where symbol in ("AAPL") and startDate = "' + startDate + '" and endDate = "' + endDate + '"');
 
@@ -23,9 +23,13 @@ function getData() {
 	
     $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
         .done(function (data) {
+	// this method obviously needs some extraction and class structure - currently spiking algorithm and data format
 			var theThing = data.query
 			var results = theThing
 			var date = new Date();
+			var weeklyQuotes = Array();
+			var currWeeklyQuote;
+			var weeklyQuoteCurrentDay = 0; // no current quote
 			
 			console.log("Count: " + theThing.count + " retrieved on " + theThing.created )
 
@@ -37,7 +41,7 @@ function getData() {
 					var quote = results[indexedQuote];
 					dateComps = quote["date"].split("-");
 					date.setFullYear(dateComps[0], dateComps[1] - 1, dateComps[2]);
-					quote["Day"] = date.getDay(); 
+					quote["Day"] = date.getDay();
 			        console.log(indexedQuote + " : " + quote["date"] + ", " +
 						quote["Date"] + ", " +
 						quote["Open"] + ", " +
@@ -47,9 +51,43 @@ function getData() {
 						quote["Volume"] + ", " +
 						quote["Adj_Close"] + ", " +
 						quote["Day"] + ", " +
-						weekday[date.getDay()])
+						weekday[date.getDay()]);
+					// create/update current quote
+					if (currWeeklyQuote && (date.getDay() < weeklyQuoteCurrentDay)) {
+						// this is an EARLIER day, so update Open/StartDate, not Close
+						currWeeklyQuote["StartDate"] = quote["date"]
+						currWeeklyQuote["Open"] = quote["Open"]
+						currWeeklyQuote["High"] = Math.max(currWeeklyQuote["High"], quote["High"])
+						currWeeklyQuote["Low"] = Math.min(currWeeklyQuote["Low"], quote["Low"])
+						weeklyQuoteCurrentDay = date.getDay()						
+						currWeeklyQuote["Count"]++						
+					} else {
+						currWeeklyQuote = {};
+						currWeeklyQuote["StartDate"] = quote["date"]
+						currWeeklyQuote["EndDate"] = quote["Date"]
+						currWeeklyQuote["Open"] = quote["Open"]
+						currWeeklyQuote["Close"] = quote["Close"]
+						currWeeklyQuote["High"] = quote["High"]
+						currWeeklyQuote["Low"] = quote["Low"]
+						currWeeklyQuote["Count"] = 1
+						weeklyQuoteCurrentDay = date.getDay()
+						weeklyQuotes.push(currWeeklyQuote)
+					}
 			    }
 			 }
+			console.log("Resulting " + weeklyQuotes.length + " Weekly Quotes:")
+			console.log("Start, End, Open, High, Low, Close, Count")
+			
+			for (var i = 0; i < weeklyQuotes.length; i++) {
+				quote = weeklyQuotes[i]
+				console.log(quote["StartDate"] + ", " +
+					quote["EndDate"] + ", " +
+					quote["Open"] + ", " +
+					quote["High"] + ", " + 
+					quote["Low"] + ", " +
+					quote["Close"] + ", " +
+					quote["Count"]);		        
+			}
 		
 		console.log("All done.")
     });
