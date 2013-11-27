2c720913daba5d08c4424a126251a0edffae0f58
Damien Del Russo <ddelruss@mac.com>
2013-11-26 20:25:07 -0800

    Added day of week to each quote and description in output.

--- stock_price_list.js
+++ stock_price_list.js
@@ -7,10 +7,20 @@ function getData() {
     var url = "http://query.yahooapis.com/v1/public/yql";
     var symbol = "AMZN";
     // var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
+	var dateComps;
 	var startDate = '2013-01-01';
-	var endDate = '2013-11-08';
+	var endDate = '2013-11-26';
 	var data = encodeURIComponent('select * from yahoo.finance.historicaldata where symbol in ("AAPL") and startDate = "' + startDate + '" and endDate = "' + endDate + '"');
 
+	var weekday=new Array(7);
+	weekday[0]="Sunday";
+	weekday[1]="Monday";
+	weekday[2]="Tuesday";
+	weekday[3]="Wednesday";
+	weekday[4]="Thursday";
+	weekday[5]="Friday";
+	weekday[6]="Saturday";
+	
     $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
         .done(function (data) {
 			var theThing = data.query
@@ -20,10 +30,13 @@ function getData() {
 
 			results = theThing.results.quote
 			// console.log(JSON.stringify(results))
-			console.log("Start, End, Open, High, Low, Close, Volume, Adj. Close")
+			console.log("Start, End, Open, High, Low, Close, Volume, Adj. Close, Day of Week, Day Name")
 			for (var indexedQuote in results) {
 			    if (results.hasOwnProperty(indexedQuote)) {
-					var quote = results[indexedQuote]	
+					var quote = results[indexedQuote];
+					dateComps = quote["date"].split("-");
+					date = new Date(dateComps[0], dateComps[1] - 1, dateComps[2]);
+					quote["Day"] = date.getDay(); 
 			        console.log(indexedQuote + " : " + quote["date"] + ", " +
 						quote["Date"] + ", " +
 						quote["Open"] + ", " +
@@ -31,7 +44,9 @@ function getData() {
 						quote["Low"] + ", " +
 						quote["Close"] + ", " +
 						quote["Volume"] + ", " +
-						quote["Adj_Close"])
+						quote["Adj_Close"] + ", " +
+						quote["Day"] + ", " +
+						weekday[date.getDay()])
 			    }
 			 }
 		
